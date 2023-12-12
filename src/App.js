50221bd9b4e2ebd998cb9f22801ad7929f40464b
최영재 <nhd5913@gsretail.com>
2023-12-12 09:54:02 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+function App() {
+  const aa = "abc" // eslint-disable-line no-unused-vars
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body className='App-body'>
+        <p>test</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sampleComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer(props) {
+    const testDt = true
+    return (
+        <div>
+            샘플 컨포넌트
+        </div>
+    );
+}
+
+export default Footer;
